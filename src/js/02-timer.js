bbe3e08d33bcab50b2d7088c96136a37e9f7ff36
MartaBahrynivska <martabagrynivska@googlemail.com>
2023-12-09 21:38:36 -0500

    timer addleadingzero

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,97 +1,88 @@
 import flatpickr from 'flatpickr';
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import convertMs from './dataConvert';
+// import convertMs from './dataConvert';
 
-let getRef = selector => document.querySelector(selector);
-const imputDatePickerRef = getRef('#datetime-picker');
-const btnStartRef = getRef('[data-start]');
-const daysRef = getRef('[data-days]');
-const hoursRef = getRef('[data-hours]');
-const minutesRef = getRef('[data-minutes]');
-const secondsRef = getRef('[data-seconds]');
-
-
-let timeDifference = 0;
-let timerId = null;
-let formatDate = null;
-
+let intervalId = null;
+let selectedDate = null;
+let currentDate = null;
+let timeToEnd = 0;
+const refs = {
+  date: document.querySelector('#datetime-picker'),
+  button: document.querySelector('[data-start]'),
+  day: document.querySelector('[data-days]'),
+  hour: document.querySelector('[data-hours]'),
+  minut: document.querySelector('[data-minutes]'),
+  second: document.querySelector('[data-seconds]'),
+};
+//
+refs.button.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    currentDifferenceDate(selectedDates[0]);
+    selectedDate = selectedDates[0].getTime();
+    currentDate = new Date().getTime();
+    if (selectedDate > currentDate) {
+      refs.button.disabled = false;
+      Notify.success('Hello!Click on start');
+
+      return;
+    }
+    return Notify.failure('Please choose a date in the future');
   },
 };
+flatpickr(refs.date, options);
+console.log(flatpickr);
+// події
+refs.button.addEventListener('click', onStart);
 
-btnStartRef.setAttribute('disabled', true);
-
-
-flatpickr(imputDatePickerRef, options);
-
-
-btnStartRef.addEventListener('click', onBtnStart);
-
-
-window.addEventListener('keydown', e => {
-  if (e.code === 'Escape' && timerId) {
-    clearInterval(timerId);
-
-    imputDatePickerRef.removeAttribute('disabled');
-    btnStartRef.setAttribute('disabled', true);
-
-    secondsRef.textContent = '00';
-    minutesRef.textContent = '00';
-    hoursRef.textContent = '00';
-    daysRef.textContent = '00';
-  }
-});
-
-
-function onBtnStart() {
-  timerId = setInterval(startTimer, 1000);
+function onStart() {
+  intervalId = setInterval(() => {
+    currentDate = new Date().getTime();
+    if (selectedDate - currentDate <= 1000) {
+      clearInterval(intervalId);
+      refs.button.disabled = true;
+      refs.date.disabled = false;
+      return Notify.success('Timer stopped!');
+      return;
+    } else {
+      refs.button.disabled = true;
+      refs.date.disabled = true;
+      currentDate += 1000;
+      timeToEnd = selectedDate - currentDate;
+      convertMs(timeToEnd);
+    }
+  }, 1000);
 }
-
-
-function currentDifferenceDate(selectedDates) {
-  const currentDate = Date.now();
-
-  if (selectedDates < currentDate) {
-    btnStartRef.setAttribute('disabled', true);
-    return Notify.failure('Please choose a date in the future');
-  }
-
-  timeDifference = selectedDates.getTime() - currentDate;
-  formatDate = convertMs(timeDifference);
-
-  renderDate(formatDate);
-  btnStartRef.removeAttribute('disabled');
+// таймер
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.day.textContent = days;
+  refs.hour.textContent = hours;
+  refs.minut.textContent = minutes;
+  refs.second.textContent = seconds;
 }
-
-
-function startTimer() {
-  btnStartRef.setAttribute('disabled', true);
-  imputDatePickerRef.setAttribute('disabled', true);
-
-  timeDifference -= 1000;
-
-  if (secondsRef.textContent <= 0 && minutesRef.textContent <= 0) {
-    Notify.success('Time end');
-    clearInterval(timerId);
-  } else {
-    formatDate = convertMs(timeDifference);
-    renderDate(formatDate);
-  }
+// форматування значень
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  const days = addLeadingZero(Math.floor(ms / day));
 
-function renderDate(formatDate) {
-  secondsRef.textContent = formatDate.seconds;
-  minutesRef.textContent = formatDate.minutes;
-  hoursRef.textContent = formatDate.hours;
-  daysRef.textContent = formatDate.days;
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+  updateClockface({ days, hours, minutes, seconds });
+  return { days, hours, minutes, seconds };
 }
-
